5850be5dfe4884f50c886642305d5dd624f759b6
Philser <pk30.05@hotmail.de>
2019-10-15 12:35:40 +0200

    Initial commit

---
+++ src/main/Main.kt
@@ -0,0 +1,15 @@
+package main
+
+import bot.Bot
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size < 1) {
+        println("Error: No token provided")
+        exitProcess(1)
+    }
+    val apiToken = args[0]
+
+    val bot = Bot(apiToken)
+    bot.runBot()
+}
